edf166710f1d078ccefac4b2fb6eb6182285e67b
Jonas <jonasjso@protonmail.com>
2020-08-11 00:00:01 +0200

    deploy docs/

---
+++ docs/build/precache-manifest.0b9649262c7c1252d86e6792059e381b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b033fb85c931a6dc2a91a177a1b32e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f2289444474d52a1d10",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "686fefdaf01da292ff81",
+    "url": "/static/js/2.46f416a9.chunk.js"
+  },
+  {
+    "revision": "0fb0e672f1308cb5b7677cdb93fc42ab",
+    "url": "/static/js/2.46f416a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f2289444474d52a1d10",
+    "url": "/static/js/main.be253f83.chunk.js"
+  },
+  {
+    "revision": "3f809adccd7b5eb81ce7",
+    "url": "/static/js/runtime-main.6d8ceafa.js"
+  }
+]);
