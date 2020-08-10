472cfcc96430a0de3221cd9de03ea7986607ce32
Jonas <jonasjso@protonmail.com>
2020-08-11 00:34:54 +0200

    update BASE URL

---
+++ docs/precache-manifest.8187e3eb3a72e1f2f92818bf3e937155.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05a9de094c170bef6f64404cebdc69a5",
+    "url": "/nobilx/index.html"
+  },
+  {
+    "revision": "d52ae7941dfc83aab006",
+    "url": "/nobilx/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "822ef972106275b6eb0b",
+    "url": "/nobilx/static/js/2.5b040d4e.chunk.js"
+  },
+  {
+    "revision": "33ade7f87243ebea1c83e915f94afce3",
+    "url": "/nobilx/static/js/2.5b040d4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d52ae7941dfc83aab006",
+    "url": "/nobilx/static/js/main.0ddf16fc.chunk.js"
+  },
+  {
+    "revision": "2c2e3537f57a6b6eb050",
+    "url": "/nobilx/static/js/runtime-main.c9fd5811.js"
+  }
+]);
